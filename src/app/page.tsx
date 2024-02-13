bf7762e43fbbde93c8d21228d217336c1bb6cb27
Ivan Shumilov <ivanshummi@gmail.com>
2024-02-13 23:27:50 +0600

    prettier files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 /* 'use client' */
-import Board from "@/app/components/Board";
+import Board from '@/app/components/Board';
 
 export default function Index() {
   return (
     <>
       <p>Let&apos;s play a chess</p>
-      <Board/>
+      <Board />
     </>
   );
 }
