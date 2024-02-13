49f214d5869d793dbd67a39ccc19f1f76824355c
Ivan Shumilov <ivanshummi@gmail.com>
2024-02-13 21:43:59 +0600

    fix prettier

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,6 +4,6 @@ const buildEslintCommand = (filenames) =>
   `yarn next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
 const formatCommand = 'yarn prettier . --write';
 module.exports = {
-  '*.{json}': [formatCommand],
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.*': [formatCommand],
 };
